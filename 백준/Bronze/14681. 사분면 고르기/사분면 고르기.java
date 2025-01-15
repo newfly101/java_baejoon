75f0e52e9559fa257bdc7c88cc934c293c882097
Jae-Hong-Kim <62008619+newfly101@users.noreply.github.com>
2025-01-15 09:47:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        System.out.println(x>0 ? (y>0 ? 1 : 4) : (y>0 ? 2 : 3));
+    }
+}
