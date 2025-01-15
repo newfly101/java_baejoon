20a16a7fe202c1d910e9c82d433dc7f1f431ddd2
Jae-Hong-Kim <62008619+newfly101@users.noreply.github.com>
2025-01-15 09:23:05 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+
+        if (a >= 90) {
+            System.out.println("A");
+        } else if (a >= 80) {
+            System.out.println("B");
+        } else if (a >= 70) {
+            System.out.println("C");
+        } else if (a >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
